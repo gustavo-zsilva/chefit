8192d52ee0257fa136e2ae0eab39285c93f1600b
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2021-05-18 20:14:22 -0300

    First Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Landing | Chef.it</title>
+      </Head>
+
+      <div className={styles.landingContainer}>
+        
+      </div>
+    </div>
+  )
+}
