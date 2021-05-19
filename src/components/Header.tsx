363b3023d17b52ad284a19a1f42064ab18ea7094
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2021-05-18 23:50:20 -0300

    Added responsivity

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,23 +5,25 @@ import styles from '../styles/components/Header.module.css';
 export function Header() {
     return (
         <header className={styles.headerContainer}>
-            <section className={styles.logo}>
-                <Link href="/">
+            <div className={styles.content}>
+                <section className={styles.logo}>
+                    <Link href="/">
+                        <button>
+                            <img src="/assets/chef.svg" alt="Chef.it Icon" />
+                            <span>Chef.it</span>
+                        </button>
+                    </Link>
+                </section>
+                <section className={styles.buttons}>
                     <button>
-                        <img src="/assets/chef.svg" alt="Chef.it Icon" />
-                        <span>Chef.it</span>
+                        Login
                     </button>
-                </Link>
-            </section>
-            <section className={styles.buttons}>
-                <button>
-                    Login
-                </button>
-                
-                <button>
-                    Sign Up
-                </button>
-            </section>
+                    
+                    <button>
+                        Sign Up
+                    </button>
+                </section>
+            </div>
         </header>
     );
 }
